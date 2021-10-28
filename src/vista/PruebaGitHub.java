9871f681a9be7f1a81c30a223a532528a58ea324
stefgin <STEFGIN@stefginE>
2021-10-28 18:22:01 -0500

    Prueba 1

---
+++ src/vista/PruebaGitHub.java
@@ -0,0 +1,11 @@
+
+package vista;
+import javax.swing.JOptionPane;
+
+
+public class PruebaGitHub {
+    public static void main(String[]args){
+       JOptionPane.showMessageDialog(null, "Prueba Git Hub\nGil\nIngenieria de Software");
+    }
+    
+}
